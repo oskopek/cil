950e2a3c202f1f5f8d8ca9f28a2d1daeed283f15
Ondrej Skopek <skopekondrej@gmail.com>
2018-02-25 14:55:29 +0100

    Data script

---
+++ data_in/download_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+echo "Make sure you are on the ETHZ VPN."
+echo ""
+
+wget "http://www.da.inf.ethz.ch/teaching/2017/CIL/material/exercise/twitter-datasets.zip"
+unzip twitter-datasets.zip
+rm twitter-datasets.zip
