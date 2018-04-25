dd03133c2bae1d09482496d2c9133d5efc764ac2
Ondrej Skopek <skopekondrej@gmail.com>
2018-04-25 10:50:42 +0200

    Added preprocessing

--- data_in/download_data.sh
+++ data_in/download_data.sh
@@ -5,6 +5,6 @@ set -e
 echo "Make sure you are on the ETHZ VPN."
 echo ""
 
-wget "http://www.da.inf.ethz.ch/teaching/2017/CIL/material/exercise/twitter-datasets.zip"
+wget "http://www.da.inf.ethz.ch/teaching/2018/CIL/material/exercise/twitter-datasets.zip"
 unzip twitter-datasets.zip
 rm twitter-datasets.zip
