52e99af00ebca41ddeac5e1fffe898910f097e04
Michael <michael.wiegner@gmail.com>
2018-06-29 10:53:38 +0200

    Added GloVe code

--- Glove/glove_embeddings.py
+++ Glove/glove_embeddings.py
@@ -1,32 +1,36 @@
-#If you want to create the word embeddings yourself, follow the tutorial on https://github.com/dalab/lecture_cil_public/tree/master/exercises/ex6
-#Keep compute_embedding, if you want to use mine
+#This code is used to run logistic regression or the random forest classifier on the embedding of a tweet.
+#To get an embeddings for the whole tweet we are averaging glove word embeddings.
 
-#TODO
-#Check which embedding to use xs or ys
-#Check if logistic regression has a regularizer
-#Train the embeddings with the full data set (so far only done with the small one)
-#Maybe try a higher embedding dimension
-#What to do if we don't have an embedding for all the words in the tweet
+#The Stanford University already has precomputed glove word embeddings for twitter (https://nlp.stanford.edu/projects/glove/)
+#We also have code to compute word embeddings ourselves and basically followed the tutorial from the CIL tutoril
+#on https://github.com/dalab/lecture_cil_public/tree/master/exercises/ex6
 
-
-#!/usr/bin/env python3
-from scipy.sparse import * 
 import numpy as np
 import pickle
-import random
 import sys
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from numpy import genfromtxt
 
 #Parameters
-compute_embedding = False
-validation_data_size = 10000
-embedding_dim = 20
-epochs = 20
-full_data_suffix = '' #To decide if we use the full data for training, for full data specify: '_full'
-selected_embedding = 'arr_0' # Choose 'arr_1' for ys
-output_file_name = 'results.csv'
+#use the standford word embedding
+use_stanford = False
 
-#Code taken from the tutorial
+#if we want to compute the word embeddings ourselves, is only considered if use_stanford is false
+compute_embedding = True
+#how many dimensions the embedding has which should be computed
+embedding_dim = 200
+#how many epochs are used to compute the word embedding
+epochs = 20
+#which embedding from glove is chosen
+selected_embedding = 'arr_0' #Choose 'arr_1' for ys
+output_file_name = 'results.csv'
+#We observed that the length of negative tweets is in general higher, if useLen is true we are using the "number of words"-information in ML models
+useLen = False
+#which classifier to use: True means to use logistic regression, False leads to using random forests
+use_logistic_regression = True
+
+#Method taken from the CIL tutorial https://github.com/dalab/lecture_cil_public/tree/master/exercises/ex6
 def compute_glove_embeddings():
     print("loading cooccurrence matrix")
     with open('cooc.pkl', 'rb') as f:
@@ -45,7 +49,7 @@ def compute_glove_embeddings():
     alpha = 3 / 4
 
     for epoch in range(epochs):
-        print("epoch {}".format(epoch))
+        print("epoch " + str(epoch), flush=True)
         for ix, jy, n in zip(cooc.row, cooc.col, cooc.data):
             logn = np.log(n)
             fn = min(1.0, (n / nmax) ** alpha)
@@ -55,67 +59,121 @@ def compute_glove_embeddings():
             ys[jy, :] += scale * x
     np.savez('embeddings', xs, ys)
 
-def read_input_data(filename, matrix_list, embeddings, vocab, is_test_data, label):
-    with open(filename, 'r+', encoding="utf-8") as f: #todo do the same for the covariance matrix
+
+def read_input_data(filename, matrix_list, embeddings, vocab, is_test_data, labels = None):
+    lineID = 0
+    with open(filename, 'r+', encoding="utf-8") as f:
         for line in f:
             tokens = [vocab.get(t, -1) for t in line.strip().split()]
+            if useLen:
+                sentLen = len(tokens)
             tokens = [t for t in tokens if t >= 0]
-            #at the moment: if we don't have an embedding for any of the words in the tweet -> don't use them for training (are not that many so far)
+            #At the moment: if we do not have an embedding for any of the words in the tweet -> do not use them for training (are not that many)
+            #Compute the avrerage
             if(len(tokens) > 0):
                 result = np.zeros(embedding_dim)
                 for t in tokens:
                     result += embeddings[t]
                 result = result / len(tokens)
+                if useLen:
+                    result = np.append(result, sentLen)
                 if not is_test_data:
-                    result = np.append(result, label)
+                   result = np.append(result, labels[lineID])
                 matrix_list.append(result)
             elif is_test_data:
-                matrix_list.append(np.zeros(embedding_dim))
+                zero_embedding = np.zeros(embedding_dim)
+                if useLen:
+                    zero_embedding = np.append(zero_embedding, 0)
+                matrix_list.append(zero_embedding)
                 print('Did not find this an embedding for any of the words')
+            lineID = lineID + 1
+
+def str_to_bool(string):
+  return string.lower() in ("yes", "true", "t", "1")
 
 def main():
-    if compute_embedding:
-        print('Compute embeddings')
-        compute_glove_embeddings()
-    #compute a mapping from the word to the index (to be able to get the right embedding for a given word)
-    vocab = dict()
-    with open('vocab_cut.txt') as f:
-        for idx, line in enumerate(f):
-            vocab[line.strip()] = idx
-    npzfile = np.load('embeddings.npz')
+    #Check if any command line parameter was added. If so, use stanford embeddings.
+    global use_stanford
+    global output_file_name
+    global use_logistic_regression
 
-    xs = npzfile[selected_embedding]
+    if (len(sys.argv) <= 1):
+        use_stanford = True
+        use_logistic_regression = False
+        output_file_name = 'results_stanford.csv'
+    elif(len(sys.argv) == 3):
+        use_logistic_regression = str_to_bool(sys.argv[1])
+        use_stanford = str_to_bool(sys.argv[2])
+
+    if compute_embedding and not use_stanford:
+        print('Compute and save embeddings')
+        compute_glove_embeddings()
+        #Compute a mapping from the word to the index (to be able to get the right embedding for a given word)
+
+    if use_stanford:
+        print("Loading Stanford 200D Vocab...")
+        stanford_label = genfromtxt('glove.twitter.27B.200d.txt', delimiter=' ', encoding="utf8", usecols=(0),dtype=None)
+        print("Loading Stanford 200D Embeddings...")
+        vocab = dict()
+        for ii in range(stanford_label.shape[0]):
+            vocab[stanford_label[ii]] = ii
+
+        stanford_data = genfromtxt('glove.twitter.27B.200d.txt', delimiter=' ', encoding="utf8")
+        stanford_data = stanford_data[:,1:]
+        print(stanford_data.shape)
+
+        glove_words_embeddings = stanford_data
+        print("Embeddings Loaded")
+
+    else:
+        vocab = dict()
+        with open('vocab_cut.txt') as f:
+            for idx, line in enumerate(f):
+                vocab[line.strip()] = idx
+        npzfile = np.load('embeddings.npz')
+        glove_words_embeddings = npzfile[selected_embedding]
+
+
     matrix_list = []
     print('Reading input')
-    read_input_data('train_neg' + full_data_suffix + '.txt', matrix_list, xs, vocab, False, 0)
-    read_input_data('train_pos' + full_data_suffix + '.txt', matrix_list, xs, vocab, False, 1)
-    input_data = np.array(matrix_list)
-    print(input_data.shape)
-    np.random.seed(42)
-    np.random.shuffle(input_data)
-    logisticRegr = LogisticRegression()
-    validation_data = input_data[0:validation_data_size,0:embedding_dim]
-    validation_labels = input_data[0:validation_data_size,embedding_dim:]
-    training_data = input_data[validation_data_size:,0:embedding_dim]
-    training_labels = input_data[validation_data_size:,embedding_dim:]
-    logisticRegr.fit(training_data,training_labels)
+    train_labels = genfromtxt('train_label.txt')
+    read_input_data('train_sent.txt', matrix_list, glove_words_embeddings, vocab, False, train_labels) #label deactivated
+    train_sentences = np.array(matrix_list)
+    train_labels = train_sentences[:,-1]
+    train_sentences = train_sentences[:, :-1]
 
-    print(logisticRegr.score(validation_data, validation_labels))
+    matrix_list = []
+    eval_labels = genfromtxt('eval_label.txt')
+    read_input_data('eval_sent.txt', matrix_list, glove_words_embeddings, vocab, False, eval_labels)  # label deactivated
+    eval_sentences = np.array(matrix_list)
+    eval_labels = eval_sentences[:, -1]
+    eval_sentences = eval_sentences[:, :-1]
+
+    if useLen:
+        global embedding_dim
+        embedding_dim = embedding_dim + 1
+    print(train_sentences.shape)
+
+    if use_logistic_regression:
+        classifier = LogisticRegression()
+    else:
+        classifier = RandomForestClassifier(128, n_jobs = -1)
+
+    classifier.fit(train_sentences, train_labels)
+    print(classifier.score(eval_sentences, eval_labels))
+
+    if useLen:
+        embedding_dim = embedding_dim - 1
     test_data = []
-    read_input_data('test_data.txt', test_data, xs, vocab, True, 0)
-    to_predict_data = np.array(test_data)  
-    print(to_predict_data.shape)    
-    predicted_results = logisticRegr.predict(to_predict_data)
+    read_input_data('test_data.txt', test_data, glove_words_embeddings, vocab, True, None)
+    to_predict_data = np.array(test_data)
+    print(to_predict_data.shape)
+    predicted_results = classifier.predict(to_predict_data)
     predicted_results[predicted_results == 0] = -1
     #Concat IDs with result
     output = np.concatenate((np.expand_dims(np.arange(1,len(predicted_results)+1),0),np.expand_dims(predicted_results,0)), axis=0).T
     #Save to file
     np.savetxt(output_file_name, output, delimiter=',', fmt = "%d,%d", header="Id,Prediction", comments='')
-
-##TODO Read in the data by words
-##For each word check the word embedding
-##Average the embeddings
-##Do logistic regression with sklearn based on the input
 
 if __name__ == '__main__':
     main()
---
+++ Glove/run.sh
@@ -0,0 +1,31 @@
+#Adapted version of the solution to excercise 6
+#Note: make sure to have data in data_in folder before running
+
+#Splitting combined data to make it easier to read data in program
+cat ../data_in/twitter-datasets/train_data.txt | cut -c 3- > train_sent.txt
+cat ../data_in/twitter-datasets/train_data.txt | cut -c -1 > train_label.txt
+cat ../data_in/twitter-datasets/eval_data.txt | cut -c 3- > eval_sent.txt
+cat ../data_in/twitter-datasets/eval_data.txt | cut -c -1 > eval_label.txt
+
+#Generating vocab, same as Ex. 6 solution
+cat train_sent.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab.txt
+
+cat vocab.txt | sed "s/^\s\+//g" | sort -rn | grep -v "^[1234]\s" | cut -d' ' -f2 > vocab_cut.txt
+
+#Download relevant stanford embeddings, clean up rest
+wget http://nlp.stanford.edu/data/glove.twitter.27B.zip
+unzip glove.twitter.27B.zip
+rm glove.twitter.27B.zip
+rm glove.twitter.27B.25d.txt
+rm glove.twitter.27B.50d.txt
+rm glove.twitter.27B.100d.txt
+
+#Pickle the vocabulary and build cooc matrix (also from Ex. 6 solution)
+python3 pickle_vocab.py	
+python3 cooc.py
+
+#Run the glove embedding sentiment classification using own embeddings that will be trained
+python3 glove_embeddings.py
+
+#Run the glove embedding sentiment classification using stanford embeddings
+python3 glove_embeddings.py useStanford
